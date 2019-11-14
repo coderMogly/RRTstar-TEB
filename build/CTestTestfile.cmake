429dec5d854edbc38c629c5f554b0fe458788160
Anirudh Yadav <anirudhyadav1001@gmail.com>
2019-11-15 00:00:52 +0530

    removed build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chacha/glocal/src
-# Build directory: /home/chacha/glocal/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(teb_local_planner_tutorials)
-SUBDIRS(rrt_planner)
--- build/rrt_planner/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/chacha/glocal/src/rrt_planner/include".split(';') if "/home/chacha/glocal/src/rrt_planner/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "costmap_2d;geometry_msgs;nav_core;navfn;nav_msgs;pluginlib;roscpp;tf;base_local_planner".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lrrt_planner".split(';') if "-lrrt_planner" != "" else []
-PROJECT_NAME = "rrt_planner"
-PROJECT_SPACE_DIR = "/home/chacha/glocal/devel"
-PROJECT_VERSION = "0.0.0"
